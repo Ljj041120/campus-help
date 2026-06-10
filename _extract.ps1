17c2fdd91ac49eadfa31b0c147a06f95a0b7c711
CampusHelp Team <student@campushelp.com>
2026-06-10 23:53:52 +0800

    移除实验报告相关文件，更新.gitignore

--- _extract.ps1
+++
@@ -1,7 +0,0 @@
-$word = New-Object -ComObject Word.Application
-$word.Visible = $false
-$doc = $word.Documents.Open("C:\Users\ASUS\Desktop\综合实训1\《软件工程综合实训1》实验指导书20260507.doc")
-$text = $doc.Content.Text
-$doc.Close()
-$word.Quit()
-Write-Host $text
